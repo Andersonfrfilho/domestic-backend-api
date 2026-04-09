7243f1e04190f61a32c5c27ab9d7f30296a55299
Anderson <andersonfrfilho@gmail.com>
2026-04-09 20:30:27 -0300

    feat: adjust logger interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { CacheModule } from '@adatechnology/cache';
 import { HttpModule } from '@adatechnology/http-client';
-import { LoggerModule, RequestContextMiddleware } from '@adatechnology/logger';
+import {
+  HTTP_LOGGING_INTERCEPTOR,
+  LoggerModule,
+  RequestContextMiddleware,
+} from '@adatechnology/logger';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { register as tsConfigPathsRegister } from 'tsconfig-paths';
 
 import { ConfigModule } from '@config/config.module';
@@ -10,15 +15,15 @@ import { HealthModule } from '@modules/health/health.module';
 
 import * as tsConfig from '../tsconfig.json';
 
+import { CategoryModule } from './modules/category/category.module';
+import { DocumentModule } from './modules/document/document.module';
+import { NotificationModule } from './modules/notification/notification.module';
+import { ProviderModule } from './modules/provider/provider.module';
+import { ReviewModule } from './modules/review/review.module';
+import { ServiceModule } from './modules/service/service.module';
+import { ServiceRequestModule } from './modules/service-request/service-request.module';
 import { SharedModule } from './modules/shared/shared.module';
 import { UserModule } from './modules/user/user.module';
-import { CategoryModule } from './modules/category/category.module';
-import { ServiceModule } from './modules/service/service.module';
-import { ProviderModule } from './modules/provider/provider.module';
-import { ServiceRequestModule } from './modules/service-request/service-request.module';
-import { ReviewModule } from './modules/review/review.module';
-import { NotificationModule } from './modules/notification/notification.module';
-import { DocumentModule } from './modules/document/document.module';
 
 const compilerOptions = tsConfig.compilerOptions;
 
@@ -28,9 +33,18 @@ tsConfigPathsRegister({
 });
 
 @Module({
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useExisting: HTTP_LOGGING_INTERCEPTOR,
+    },
+  ],
   imports: [
     ConfigModule,
-    LoggerModule.forRoot({ level: process.env.LOG_LEVEL || 'info' }),
+    LoggerModule.forRoot({
+      level: process.env.LOG_LEVEL || 'info',
+      interceptorExcludedPaths: ['/health'],
+    }),
     CacheModule.forRoot({ isGlobal: true }),
     HttpModule.forRoot({}, { provide: 'HTTP_PROVIDER' }),
     SharedModule,
